bbe7ba34d12e19afeb564332904af527967043e5
wlillisdev <williamlillis100@gmail.com>
2022-07-06 09:43:22 +0000

    instslled summernote and migrated

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+# Register Post models.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    
+    summernote_fields = ('content')
+
