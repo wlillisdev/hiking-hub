6097d8fcf6c877db0849b70328847e75ef3b4d1c
wlillisdev <williamlillis100@gmail.com>
2022-07-08 10:23:50 +0000

    user able to comment on blog post added

--- hikinghub/settings.py
+++ hikinghub/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
@@ -57,6 +58,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
