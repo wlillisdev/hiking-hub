858c77df3d423faca7c5c074478415f48b94b472
wlillisdev <williamlillis100@gmail.com>
2022-07-23 18:00:47 +0000

    add doc strings to aps.py

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,12 @@
+'''
+    imports
+'''
 from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    '''
+        class to define app name
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
